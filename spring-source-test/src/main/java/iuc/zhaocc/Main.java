2618dfc112e2e109654a2a7766b89e1bf8426917
zhaocc <2122446868@qq.com>
2026-07-15 16:30:57 +0800

    1

--- spring-source-test/src/main/java/iuc/zhaocc/Main.java
+++ spring-source-test/src/main/java/iuc/zhaocc/Main.java
@@ -3,13 +3,21 @@ package iuc.zhaocc;
 import iuc.zhaocc.po.Person;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.lang.management.ManagementFactory;
+
 /**
  * @author zhaocc
  * @version 1.0
  * @date 2026-07-15
  */
-public class App {
+public class Main {
 	public static void main(String[] args) {
+		System.out.println(
+				ManagementFactory.getRuntimeMXBean().getInputArguments()
+		);
+		System.out.println(Main.class.getProtectionDomain()
+				.getCodeSource()
+				.getLocation());
 		System.out.println("123");
 		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("bean.xml");
 		Person person = (Person) applicationContext.getBean("person");
